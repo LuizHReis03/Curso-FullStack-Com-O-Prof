a71291520d01454467d5ef39a95b77a49e25730d
LuizHReis03 <luizhenriquereis70@gmail.com>
2024-06-01 14:03:44 -0300

    Adicionando Aulas

---
+++ aula02_com_o_prof/exercicio4.js
@@ -0,0 +1,7 @@
+let pessoa = {
+    nome: "Luiz",
+    cpf: "123.123.123-45",
+    cidade: "João Pessoa"
+};
+
+console.log(`O nome é: ${pessoa.nome} | o cpf é: ${pessoa.cpf} | e a cidade é: ${pessoa.cidade} `);
